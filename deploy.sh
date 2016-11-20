fb9b279ed4e1b1ccbd534afc458855d4df5c9f95
Adrien Desbiaux <adrien.desbiaux@gmail.com>
2016-11-20 16:53:58 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#### Description: Deploy serverless services to AWS.
+#### Dependencies: Depends on aws cli credentials or global AWS_* env variables.
+#### Written by: Nick den Engelsman - nick@bandlab.com on 2016-10
+
+set -e
+
+AWS_REGION="ahahahahah"
+SLS_STAGE="ohhoh"
